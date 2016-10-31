1d0ed7f49bcb092664bb71eccc6b5fa812008ae9
Alan Gomes <alanhr2@gmail.com>
2016-10-30 22:38:41 -0200

    config flow load image

--- app/src/layouts/Layout.js
+++ app/src/layouts/Layout.js
@@ -1,15 +1,18 @@
 /* @flow */
 
 import React from 'react';
-import FirstComponent from './components/firstcomponent/FirstComponent';
+import logo from 'images/Logo_MA_Blocada_Colorida.png';
+
+import FirstComponent from '../components/firstcomponent/FirstComponent';
 import style from './App.styl';
 
-const App = () => (
+const Layout = () => (
 	<div>
+		<img src={logo} alt="Teste" />
 		<h1 className={style.exampleClass}>Good to go!</h1>
 		<p className={style.anotherExampleClass}>Uhul</p>
 		<FirstComponent />
 	</div>
 );
 
-export default App;
+export default Layout;
